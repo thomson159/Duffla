9443a68cf3a3cfa2255202771f8b7a03ed1fdbd0
Michał Chęciński <michalchecinski@MacBook-Pro-Micha.local>
2025-04-14 19:44:04 +0200

    without customers opinion

--- components/Homepage/Examples.tsx
+++ components/Homepage/Examples.tsx
@@ -21,38 +21,38 @@ export const Examples = () => {
       </div>
       <div className="max-w-[1440px] mx-auto -mt-4 md:mt-0">
         <div className="relative pb-20 px-4 lg:px-28 z-10">
-          <div className="flex flex-wrap gap-y-8 md:gap-x-[3%] mt-16">
-            <ExampleCard
+          {/* <div className="flex flex-wrap gap-y-8 md:gap-x-[3%] mt-16"> */}
+            {/* <ExampleCard
               icon="coinbase"
               title="Zofia"
               subtitle=""
               description="Praca z tym architektem to czysta przyjemność! Od pierwszego spotkania wykazał się ogromnym profesjonalizmem i zrozumieniem naszych potrzeb. Każdy etap projektu był dokładnie dopracowany, a komunikacja przebiegała bez zarzutu. Efekt końcowy przerósł nasze oczekiwania – stworzył przestrzeń, która idealnie łączy funkcjonalność z estetyką. Zdecydowanie polecamy!"
               type="interaction"
               className="w-full md:w-[31.333%]"
-            />
-            <ExampleCard
+            /> */}
+            {/* <ExampleCard
               icon="moonpay-purple"
               title="Mateusz"
               subtitle=""
               description="Zdecydowanie najlepsza współpraca, jaką mieliśmy przy realizacji naszego projektu! Architekt wykazał się nie tylko ogromną wiedzą, ale również kreatywnością i elastycznością. Każda nasza sugestia była uwzględniona, a projekt dostosowany do naszych indywidualnych potrzeb. Cieszymy się, że zdecydowaliśmy się na współpracę i nie możemy się doczekać, by cieszyć się gotową przestrzenią."
               type="live-app"
               className="w-full md:w-[31.333%]"
-            />
-            <ExampleCard
+            /> */}
+            {/* <ExampleCard
               icon="lido"
               title="Marek"
               subtitle=""
               description="Profesjonalizm, precyzyjność i zaangażowanie – to słowa, które najlepiej opisują naszą współpracę z tym architektem. Już na etapie planowania czuliśmy się pewnie, wiedząc, że nasz projekt jest w rękach specjalisty. Efekt końcowy jest niesamowity – architekt stworzył dla nas przestrzeń, która doskonale odpowiada naszym wymaganiom, a do tego jest piękna i funkcjonalna."
               type="live-app"
               className="w-full md:w-[31.333%]"
-            />
-            <div className="md:hidden w-full text-p text-grey-500 mt-4">
+            /> */}
+            {/* <div className="md:hidden w-full text-p text-grey-500 mt-4"> */}
               {/* I wiele więcej... */}
-            </div>
-          </div>
-          <div className="hidden md:block text-p text-grey-500 text-right mt-4">
+            {/* </div> */}
+          {/* </div> */}
+          {/* <div className="hidden md:block text-p text-grey-500 text-right mt-4"> */}
             {/* I wiele więcej... */}
-          </div>
+          {/* </div> */}
         </div>
       </div>
     </div>
